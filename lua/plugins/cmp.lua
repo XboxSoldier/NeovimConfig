2a0f62df12e9f697bbaa5ec41b982acbbdee138b
Cheng Jingxuan <hbxycjx@outlook.com>
2024-10-21 18:50:26 +0800

    Resolved Copilot and Cmp Key Confliction

--- lua/plugins/cmp.lua
+++ lua/plugins/cmp.lua
@@ -11,20 +11,20 @@ cmp.setup({
     end,
   },
   mapping = cmp.mapping.preset.insert({
-    ["<Tab>"] = cmp.mapping(function(fallback)
-      if cmp.visible() then
-        cmp.select_next_item()
-      else
-        fallback()
-      end
-    end, { "i", "s" }),
-    ["<S-Tab>"] = cmp.mapping(function(fallback)
-      if cmp.visible() then
-        cmp.select_prev_item()
-      else
-        fallback()
-      end
-    end, { "i", "s" }),
+    --  ["<Tab>"] = cmp.mapping(function(fallback)
+    --    if cmp.visible() then
+    --      cmp.select_next_item()
+    --    else
+    --      fallback()
+    --    end
+    --  end, { "i", "s" }),
+    --  ["<S-Tab>"] = cmp.mapping(function(fallback)
+    --    if cmp.visible() then
+    --      cmp.select_prev_item()
+    --    else
+    --      fallback()
+    --    end
+    --  end, { "i", "s" }),
 
     ["<C-u>"] = cmp.mapping.scroll_docs(-4),
     ["<C-d>"] = cmp.mapping.scroll_docs(4),
